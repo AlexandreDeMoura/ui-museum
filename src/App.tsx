ce051d03f1ac110d1817538f2e49f5ec974275c2
AlexandreDeMoura <alexandredemourapro@gmail.com>
2023-02-22 12:29:50 +0100

    feat: test of react-contenteditable

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
+import sanitizeHtml from "sanitize-html";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [content, setContent] = React.useState("");
+
+  const onContentChange = React.useCallback(
+    (
+      event: ContentEditableEvent | React.FocusEvent<HTMLDivElement, Element>
+    ) => {
+      const sanitizeConf = {
+        allowedTags: ["b", "i", "a", "p", "h1", "h2", "h3"],
+        allowedAttributes: { a: ["href"] },
+      };
+
+      setContent(sanitizeHtml(event.currentTarget.innerHTML, sanitizeConf));
+    },
+    []
   );
-}
+
+  return (
+    <ContentEditable
+      onChange={onContentChange}
+      onBlur={(event) => onContentChange(event)}
+      html={content}
+    />
+  );
+};
 
 export default App;
