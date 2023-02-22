0aca13598502d9ec773eaea7c52801050481b134
AlexandreDeMoura <alexandredemourapro@gmail.com>
2023-02-22 15:12:44 +0100

    feat: add new editable block when pressing "Enter"

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
 import sanitizeHtml from "sanitize-html";
 
 const App = () => {
-  const [content, setContent] = React.useState("");
+  const [contents, setContents] = React.useState([""]);
 
-  const onContentChange = React.useCallback(
-    (
-      event: ContentEditableEvent | React.FocusEvent<HTMLDivElement, Element>
-    ) => {
-      const sanitizeConf = {
-        allowedTags: ["b", "i", "a", "p", "h1", "h2", "h3"],
-        allowedAttributes: { a: ["href"] },
-      };
+  const handleChange = (
+    index: number,
+    event: ContentEditableEvent | React.FocusEvent<HTMLDivElement, Element>
+  ) => {
+    const sanitizeConf = {
+      allowedTags: ["b", "i", "a", "p", "h1", "h2", "h3"],
+      allowedAttributes: { a: ["href"] },
+    };
+    setContents((currentContents) => {
+      const newContents = [...currentContents];
+      newContents[index] = sanitizeHtml(
+        event.currentTarget.innerHTML,
+        sanitizeConf
+      );
+      return newContents;
+    });
+  };
 
-      setContent(sanitizeHtml(event.currentTarget.innerHTML, sanitizeConf));
-    },
-    []
-  );
+  const handleKeyDown = (
+    index: number,
+    event: React.KeyboardEvent<HTMLDivElement>
+  ) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      setContents((currentContents) => {
+        const newContents = [...currentContents];
+        newContents.splice(index + 1, 0, "");
+        return newContents;
+      });
+    }
+  };
 
   return (
-    <ContentEditable
-      onChange={onContentChange}
-      onBlur={(event) => onContentChange(event)}
-      html={content}
-    />
+    <main className="h-screen flex justify-center pt-10 bg-slate-50">
+      <div className="w-8/12 bg-white space-y-2">
+        {contents.map((content, index) => (
+          <ContentEditable
+            className="border border-black p-2"
+            onKeyDown={(event) => handleKeyDown(index, event)}
+            onChange={(event) => handleChange(index, event)}
+            html={content}
+          />
+        ))}
+      </div>
+    </main>
   );
 };
 
