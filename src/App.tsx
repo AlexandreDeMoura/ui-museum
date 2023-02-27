2e8074c699c7453a1f3a6c109d9cef6ecf457cd9
AlexandreDeMoura <alexandredemourapro@gmail.com>
2023-02-27 12:40:02 +0100

    refactor: convert string list to nodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useRef, useState } from "react";
 import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
 import sanitizeHtml from "sanitize-html";
 
 const App = () => {
-  const [contents, setContents] = React.useState([""]);
+  const nodesRef: any = useRef([]);
+  const [nodes, setNodes] = useState<any[]>([
+    <ContentEditable
+      innerRef={(elem: any) => (nodesRef.current[0] = elem)}
+      className="border border-black p-2"
+      onKeyDown={(event) => handleKeyDown(0, event)}
+      onChange={(event) => handleChange(0, event)}
+      html={""}
+    />,
+  ]);
 
   const handleChange = (
     index: number,
@@ -13,11 +22,16 @@ const App = () => {
       allowedTags: ["b", "i", "a", "p", "h1", "h2", "h3"],
       allowedAttributes: { a: ["href"] },
     };
-    setContents((currentContents) => {
-      const newContents = [...currentContents];
-      newContents[index] = sanitizeHtml(
-        event.currentTarget.innerHTML,
-        sanitizeConf
+    setNodes((currentNodes) => {
+      const newContents = [...currentNodes];
+      newContents[index] = (
+        <ContentEditable
+          innerRef={(elem: any) => (nodesRef.current[index] = elem)}
+          className="border border-black p-2"
+          onKeyDown={(event) => handleKeyDown(index, event)}
+          onChange={(event) => handleChange(index, event)}
+          html={sanitizeHtml(event.currentTarget.innerHTML, sanitizeConf)}
+        />
       );
       return newContents;
     });
@@ -29,9 +43,18 @@ const App = () => {
   ) => {
     if (event.key === "Enter") {
       event.preventDefault();
-      setContents((currentContents) => {
-        const newContents = [...currentContents];
-        newContents.splice(index + 1, 0, "");
+      setNodes((currentNodes) => {
+        const newContents = [...currentNodes];
+        const newNode = (
+          <ContentEditable
+            innerRef={(elem: any) => (nodesRef.current[index + 1] = elem)}
+            className="border border-black p-2"
+            onKeyDown={(event) => handleKeyDown(index + 1, event)}
+            onChange={(event) => handleChange(index + 1, event)}
+            html={""}
+          />
+        );
+        newContents.splice(index + 1, 0, newNode);
         return newContents;
       });
     }
@@ -39,16 +62,7 @@ const App = () => {
 
   return (
     <main className="h-screen flex justify-center pt-10 bg-slate-50">
-      <div className="w-8/12 bg-white space-y-2">
-        {contents.map((content, index) => (
-          <ContentEditable
-            className="border border-black p-2"
-            onKeyDown={(event) => handleKeyDown(index, event)}
-            onChange={(event) => handleChange(index, event)}
-            html={content}
-          />
-        ))}
-      </div>
+      <div className="w-8/12 bg-white space-y-2">{nodes}</div>
     </main>
   );
 };
