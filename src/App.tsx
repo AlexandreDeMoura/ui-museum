2607da8ffc7648320082961085697875a38a6481
AlexandreDeMoura <alexandredemourapro@gmail.com>
2023-02-28 16:08:06 +0100

    refactor: Using unique id instead of index properties for node list

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
 import sanitizeHtml from "sanitize-html";
+import { v4 as uuidv4 } from "uuid";
+
+type Node = {
+  component: JSX.Element;
+  id: string;
+  isFocus: boolean;
+};
 
 const App = () => {
   const nodesRef: any = useRef([]);
-  const [nodes, setNodes] = useState<any[]>([
-    <ContentEditable
-      innerRef={(elem: any) => (nodesRef.current[0] = elem)}
-      className="border border-black p-2"
-      onKeyDown={(event) => handleKeyDown(0, event)}
-      onChange={(event) => handleChange(0, event)}
-      html={""}
-    />,
+  const [nodes, setNodes] = useState<Node[]>([
+    {
+      component: (
+        <ContentEditable
+          innerRef={(elem: any) => (nodesRef.current[0] = elem)}
+          className="border border-black p-2"
+          onKeyDown={(event) => handleKeyDown("first-node", event)}
+          onChange={(event) => handleChange("first-node", event)}
+          html={""}
+        />
+      ),
+      id: "first-node",
+      isFocus: true,
+    },
   ]);
 
+  useEffect(() => {
+    //console.log(nodesRef.current[0].focus());
+  });
+
   const handleChange = (
-    index: number,
+    nodeId: string,
     event: ContentEditableEvent | React.FocusEvent<HTMLDivElement, Element>
   ) => {
     const sanitizeConf = {
@@ -23,46 +40,59 @@ const App = () => {
       allowedAttributes: { a: ["href"] },
     };
     setNodes((currentNodes) => {
-      const newContents = [...currentNodes];
-      newContents[index] = (
-        <ContentEditable
-          innerRef={(elem: any) => (nodesRef.current[index] = elem)}
-          className="border border-black p-2"
-          onKeyDown={(event) => handleKeyDown(index, event)}
-          onChange={(event) => handleChange(index, event)}
-          html={sanitizeHtml(event.currentTarget.innerHTML, sanitizeConf)}
-        />
-      );
-      return newContents;
+      const newNodes = [...currentNodes];
+      const nodeIndex = newNodes.findIndex((node) => node.id === nodeId);
+      newNodes[nodeIndex] = {
+        component: (
+          <ContentEditable
+            //innerRef={(elem: any) => (nodesRef.current[index] = elem)}
+            className="border border-black p-2"
+            onKeyDown={(event) => handleKeyDown(newNodes[nodeIndex].id, event)}
+            onChange={(event) => handleChange(newNodes[nodeIndex].id, event)}
+            html={sanitizeHtml(event.currentTarget.innerHTML, sanitizeConf)}
+          />
+        ),
+        id: newNodes[nodeIndex].id,
+        isFocus: true,
+      };
+      return newNodes;
     });
   };
 
   const handleKeyDown = (
-    index: number,
+    nodeId: string,
     event: React.KeyboardEvent<HTMLDivElement>
   ) => {
     if (event.key === "Enter") {
       event.preventDefault();
       setNodes((currentNodes) => {
-        const newContents = [...currentNodes];
-        const newNode = (
-          <ContentEditable
-            innerRef={(elem: any) => (nodesRef.current[index + 1] = elem)}
-            className="border border-black p-2"
-            onKeyDown={(event) => handleKeyDown(index + 1, event)}
-            onChange={(event) => handleChange(index + 1, event)}
-            html={""}
-          />
-        );
-        newContents.splice(index + 1, 0, newNode);
-        return newContents;
+        const newNodes = [...currentNodes];
+        const nodeIndex = newNodes.findIndex((node) => node.id === nodeId);
+        const newNodeId = uuidv4();
+        const newNode = {
+          id: newNodeId,
+          isFocus: true,
+          component: (
+            <ContentEditable
+              //innerRef={(elem: any) => (nodesRef.current[index + 1] = elem)}
+              className="border border-black p-2"
+              onKeyDown={(event) => handleKeyDown(newNodeId, event)}
+              onChange={(event) => handleChange(newNodeId, event)}
+              html={""}
+            />
+          ),
+        };
+        newNodes.splice(nodeIndex + 1, 0, newNode);
+        return newNodes;
       });
     }
   };
 
   return (
     <main className="h-screen flex justify-center pt-10 bg-slate-50">
-      <div className="w-8/12 bg-white space-y-2">{nodes}</div>
+      <div className="w-8/12 bg-white space-y-2">
+        {nodes.map((node) => node.component)}
+      </div>
     </main>
   );
 };
